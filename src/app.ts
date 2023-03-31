98d417aa83754f2767f26880e7255a6eda07f7ac
Joshua Permito <dwigoric@insanedevs.com>
2023-03-31 22:25:30 +0800

    src: create entry file app.ts

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import ChatBot from './lib/structures/ChatBot';
+import config from './config';
+
+require('dotenv').config();
+
+const client = new ChatBot(config)
+
+client.login(process.env.TOKEN);
