ea104ebe18405dff132f6016f068bc2388875bca
Joshua Permito <dwigoric@insanedevs.com>
2023-04-03 21:15:56 +0800

    feat(ping): implement application command

--- src/commands/General/Chat Bot Info/ping.ts
+++ src/commands/General/Chat Bot Info/ping.ts
@@ -1,5 +1,6 @@
-import { Command, container } from '@sapphire/framework';
+import { ChatInputCommand, Command, container } from '@sapphire/framework';
 import { Message } from "discord.js";
+import { isMessageInstance } from "@sapphire/discord.js-utilities";
 
 export class PingCommand extends Command {
     constructor(context: Command.Context, options: Command.Options) {
@@ -7,6 +8,25 @@ export class PingCommand extends Command {
             ...options,
             description: 'Displays bot\'s ping.',
         });
+    }
+
+    public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+        registry.registerChatInputCommand(builder => {
+           builder
+                .setName(this.name)
+                .setDescription(this.description)
+        }, {
+            guildIds: [process.env.TEST_GUILD_ID ?? ''],
+            idHints: ['1092428204653940827']
+        });
+    }
+
+    public async chatInputRun(interaction: ChatInputCommand.Interaction) {
+        const sentMessage = await interaction.reply({ content: 'Pinging...', ephemeral: true, fetchReply: true });
+
+        return isMessageInstance(sentMessage) ?
+            sentMessage.edit(`Pong! Latency is ${sentMessage.createdTimestamp - interaction.createdTimestamp}ms. API Latency is ${Math.round(container.client.ws.ping)}ms`) :
+            interaction.editReply('Failed to get ping.');
     }
 
     async messageRun(message: Message) {
