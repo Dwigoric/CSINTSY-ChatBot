a51d64d736cbb7326c00248ee1e9658937ef1b5c
Joshua Permito <dwigoric@insanedevs.com>
2023-04-04 10:23:42 +0800

    src: bridge tau-prolog to ChatBot client

--- src/lib/structures/ChatBot.ts
+++ src/lib/structures/ChatBot.ts
@@ -1,8 +1,24 @@
 import { SapphireClient } from '@sapphire/framework';
 import { ClientOptions } from "discord.js";
 
+interface tau_prolog {
+    create: (options: { limit: number }) => {
+        consult: (code: string) => void;
+        query: (code: string) => {
+            solution: () => {
+                unify: (variable: string, value: string) => void;
+            },
+            next: () => void;
+        }
+    }
+}
+
 export default class ChatBot extends SapphireClient {
+    private pl: tau_prolog;
+
     constructor(options: ClientOptions) {
         super(options);
+
+        this.pl = require('tau-prolog');
     }
 }
