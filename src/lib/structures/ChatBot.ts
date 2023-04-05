4be818ab663b0937a841083277b8c7912f63b03a
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-04-05 15:56:06 +0800

    [Add] Barebone flow of Tau Prolog

--- src/lib/structures/ChatBot.ts
+++ src/lib/structures/ChatBot.ts
@@ -1,30 +1,65 @@
-import { SapphireClient } from '@sapphire/framework';
+import { SapphireClient } from "@sapphire/framework";
 import { ClientOptions } from "discord.js";
 
-interface tau_prolog {
-    create: (options: { limit: number }) => {
-        consult: (code: string) => void;
-        query: (code: string) => {
-            solution: () => {
-                unify: (variable: string, value: string) => void;
-            },
-            next: () => void;
-        }
-    }
+interface Session {
+    consult: (code: string) => void;
+    query: (code: string) => {
+        solution: () => {
+            unify: (variable: string, value: string) => void;
+        };
+        next: () => void;
+    };
 }
 
+interface TauPrologInstance {
+    create: (options: { limit: number }) => Session;
+}
+
+const diagnosis = (session: Session) => {
+    session.consult("../../knowledgeBase.pro"),
+        {
+            success: () => {
+                session.query("member(X, [a, b, c])."),
+                    {
+                        success: () => {},
+                        fail: () => {},
+                        error: (err: unknown) => {
+                            if (err instanceof Error) {
+                                console.log(
+                                    `[ERROR in query - ChatBot.ts] ${err.message}]`
+                                );
+                            }
+                        },
+                    };
+            },
+
+            error: (err: unknown) => {
+                if (err instanceof Error) {
+                    console.log(
+                        `[ERROR in consult - ChatBot.ts] ${err.message}]`
+                    );
+                }
+            },
+        };
+};
+
 export default class ChatBot extends SapphireClient {
-    pl: tau_prolog;
+    pl: TauPrologInstance;
 
     constructor(options: ClientOptions) {
         super(options);
 
-        this.pl = require('tau-prolog');
+        this.pl = require("tau-prolog");
+    }
+
+    async start() {
+        const session: Session = this.pl.create({ limit: 1000 });
+        diagnosis(session);
     }
 }
 
-declare module 'discord.js' {
+declare module "discord.js" {
     interface Client {
-        pl: tau_prolog;
+        pl: TauPrologInstance;
     }
 }
