71c1086069b1937fa897cc9885b0d9ea3d97aab9
Joshua Permito <dwigoric@insanedevs.com>
2023-04-05 11:49:16 +0800

    src: create diagnosis handler

---
+++ src/interaction-handlers/buttons/diagnosis-handler.ts
@@ -0,0 +1,34 @@
+import {InteractionHandler, InteractionHandlerTypes, PieceContext} from "@sapphire/framework";
+import {ButtonInteraction} from "discord.js";
+
+export class DiagnosisHandler extends InteractionHandler {
+    public constructor(context: PieceContext, options: InteractionHandler.Options) {
+        super(context, {
+            ...options,
+            interactionHandlerType: InteractionHandlerTypes.Button
+        });
+    }
+
+    public override parse(interaction: ButtonInteraction) {
+        if (interaction.customId !== 'diagnosis:agree') return this.none();
+
+        return this.some();
+    }
+
+    /**
+     * This is where the questions will be asked.
+     * @param interaction   The interaction that triggered this handler.
+     */
+    public async run(interaction: ButtonInteraction) {
+        const session = this.container.client.sessions.get(interaction.user.id);
+        if (session) return;
+        this.container.client.sessions.set(interaction.user.id, interaction.channelId);
+
+        const plSession = this.container.client.pl.create({limit: 1000});
+
+        plSession.query(`
+            question(Question, Answer) :-
+                question(Question, Answer, _).
+        `);
+    }
+}
