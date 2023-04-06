d5860652cd3b9fec4d87ca967fd8369455e15fdf
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-04-06 22:45:57 +0800

    merge: resolve conflict with stashed

--- src/lib/structures/ChatBot.ts
+++ src/lib/structures/ChatBot.ts
@@ -2,88 +2,79 @@ import { SapphireClient } from "@sapphire/framework";
 import { ClientOptions, Snowflake } from "discord.js";
 
 interface Session {
-    consult: (code: string) => void;
-    query: (code: string) => {
-        solution: () => {
-            unify: (variable: string, value: string) => void;
-        };
-        next: () => void;
-    };
+	consult: (code: string) => void;
+	query: (code: string) => {
+		solution: () => {
+			unify: (variable: string, value: string) => void;
+		};
+		next: () => void;
+	};
 }
 
 interface TauPrologInstance {
-    create: (options: { limit: number }) => Session;
+	create: (options: { limit: number }) => Session;
 }
 
-type FamilyHistory =
-    | "high_blood_pressure"
-    | "diabetes"
-    | "uti"
-    | "breast_cancer";
+type FamilyHistory = "high_blood_pressure" | "diabetes" | "uti" | "breast_cancer";
 interface PersonalData {
-    age: number;
-    biologicalSex: string;
-    height: number;
-    weight: number;
-    systolicBloodPressure: number;
-    diastolicBloodPressure: number;
-    smoking: boolean;
-    history: FamilyHistory[];
-    accomplishedHistory: boolean;
-    started: boolean;
+	age: number;
+	biologicalSex: string;
+	height: number;
+	weight: number;
+	systolicBloodPressure: number;
+	diastolicBloodPressure: number;
+	smoking: boolean;
+	history: FamilyHistory[];
+	accomplishedHistory: boolean;
+	started: boolean;
 }
 
 export default class ChatBot extends SapphireClient {
-    pl: TauPrologInstance;
+	pl: TauPrologInstance;
+	session: Session;
 
-    // Snowflake is a string, but it's a string of numbers.
-    // ChatBot#sessions is a map of user IDs to their personal data.
-    directory: Map<Snowflake, PersonalData>;
+	// Snowflake is a string, but it's a string of numbers.
+	// ChatBot#sessions is a map of user IDs to their personal data.
+	directory: Map<Snowflake, PersonalData>;
 
-    constructor(options: ClientOptions) {
-        super(options);
+	constructor(options: ClientOptions) {
+		super(options);
 
-        this.pl = require("tau-prolog");
-        this.directory = new Map();
-    }
+		this.pl = require("tau-prolog");
+		this.session = this.pl.create({ limit: 1000 });
+		this.directory = new Map();
+	}
 
-    async start() {
-        const session: Session = this.pl.create({ limit: 1000 });
-        diagnosis(session);
-    }
+	async start() {}
 }
 
 declare module "@sapphire/framework" {
-    interface SapphireClient {
-        pl: TauPrologInstance;
-        directory: Map<Snowflake, PersonalData>;
-    }
+	interface SapphireClient {
+		pl: TauPrologInstance;
+		directory: Map<Snowflake, PersonalData>;
+	}
 }
 
 function diagnosis(session: Session) {
-    session.consult("../../knowledgeBase.pro"),
-        {
-            success: () => {
-                session.query("member(X, [a, b, c])."),
-                    {
-                        success: () => {},
-                        fail: () => {},
-                        error: (err: unknown) => {
-                            if (err instanceof Error) {
-                                console.log(
-                                    `[ERROR in query - ChatBot.ts] ${err.message}]`
-                                );
-                            }
-                        },
-                    };
-            },
+	session.consult("../../knowledgeBase.pro"),
+		{
+			success: () => {
+				session.query("member(X, [a, b, c])."),
+					{
+						success: () => {},
+						fail: () => {},
+						error: (err: unknown) => {
+							if (err instanceof Error) {
+								console.log(`[ERROR in query - ChatBot.ts] ${err.message}]`);
+							}
+						},
+					};
+			},
 
-            error: (err: unknown) => {
-                if (err instanceof Error) {
-                    console.log(
-                        `[ERROR in consult - ChatBot.ts] ${err.message}]`
-                    );
-                }
-            },
-        };
+			error: (err: unknown) => {
+				if (err instanceof Error) {
+					console.log(`[ERROR in consult - ChatBot.ts] ${err.message}]`);
+				}
+			},
+		};
 }
