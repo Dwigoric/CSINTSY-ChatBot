67e4b5c2f601fefe789c08aa8babae97388c2670
Joshua Permito <dwigoric@insanedevs.com>
2023-04-06 09:33:54 +0800

    misc: check non-existence of data if trying to delete

--- src/interaction-handlers/buttons/delete-existing-data.ts
+++ src/interaction-handlers/buttons/delete-existing-data.ts
@@ -12,7 +12,14 @@ export class ExistingDataHandler extends InteractionHandler {
     public override parse(interaction: ButtonInteraction) {
         if (!['diagnosis:existing', 'diagnosis:disagree'].includes(interaction.customId)) return this.none();
         const data = this.container.client.directory.get(interaction.user.id);
-        if (data && data.started) {
+        if (!data) {
+            interaction.reply({
+                content: '❌ You have no existing diagnosis session.',
+                ephemeral: true
+            });
+            return this.none();
+        }
+        else if (data && data.started) {
             interaction.reply({
                 content: '❌ You have already started your diagnosis session.',
                 ephemeral: true
