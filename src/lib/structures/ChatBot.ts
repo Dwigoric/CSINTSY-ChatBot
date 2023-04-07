a67f109a77dab148cbfa350630a649cf1d8e18dd
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-04-07 13:54:36 +0800

    update: put additional info to personalData

--- src/lib/structures/ChatBot.ts
+++ src/lib/structures/ChatBot.ts
@@ -22,23 +22,23 @@ type FamilyHistory = "high_blood_pressure" | "diabetes" | "uti" | "breast_cancer
 interface PersonalData {
 	// Age related
 	age: number;
-	ageGroup: "infant" | "normal" | "old" | null;
+	ageGroup: string;
 
 	biologicalSex: string;
 
 	// BMI related
 	height: number;
 	weight: number;
-	bmi: number | null;
+	bmi: number;
 
 	// Fever related
 	bodyTemperature: number;
-	hasFever: boolean | null;
+	hasFever: boolean;
 
 	// BP related
 	systolicBloodPressure: number;
 	diastolicBloodPressure: number;
-	highBloodPressure: boolean | null;
+	highBloodPressure: boolean;
 
 	smoking: boolean;
 	history: FamilyHistory[];
@@ -69,28 +69,6 @@ export default class ChatBot extends SapphireClient {
 		this.session.consult("../../knowledgeBase.pro");
 		this.session.query(query);
 		console.log(this.session.answers);
-	}
-
-	setPersonalData(userInput: PersonalData) {
-		this.personalData = userInput;
-
-		// Get age group
-		if (this.personalData.age < 6) this.personalData.ageGroup = "infant";
-		else if (this.personalData.age < 5 && this.personalData.age < 60) this.personalData.ageGroup = "normal";
-		else this.personalData.ageGroup = "old";
-
-		// Get BMI
-		this.personalData.bmi = Math.ceil(this.personalData.weight / (this.personalData.height / 100) ** 2);
-
-		// Check if person has high blood pressure
-		this.personalData.highBloodPressure = this.personalData.systolicBloodPressure > 140 || this.personalData.diastolicBloodPressure > 90;
-
-		// Check if person has fever
-		this.personalData.hasFever = this.personalData.bodyTemperature > 38;
-	}
-
-	getPersonalData() {
-		return this.personalData;
 	}
 }
 
