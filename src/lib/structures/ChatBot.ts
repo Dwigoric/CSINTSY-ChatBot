4560d3daf4b5c75c73b34b5855e34121559b1cbc
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-04-07 13:43:56 +0800

    update: Save personal data to chatbot

--- src/lib/structures/ChatBot.ts
+++ src/lib/structures/ChatBot.ts
@@ -9,6 +9,9 @@ interface Session {
 		};
 		next: () => void;
 	};
+	answers: {
+		[variable: string]: string;
+	};
 }
 
 interface TauPrologInstance {
@@ -17,12 +20,26 @@ interface TauPrologInstance {
 
 type FamilyHistory = "high_blood_pressure" | "diabetes" | "uti" | "breast_cancer";
 interface PersonalData {
+	// Age related
 	age: number;
+	ageGroup: "infant" | "normal" | "old" | null;
+
 	biologicalSex: string;
+
+	// BMI related
 	height: number;
 	weight: number;
+	bmi: number | null;
+
+	// Fever related
+	bodyTemperature: number;
+	hasFever: boolean | null;
+
+	// BP related
 	systolicBloodPressure: number;
 	diastolicBloodPressure: number;
+	highBloodPressure: boolean | null;
+
 	smoking: boolean;
 	history: FamilyHistory[];
 	accomplishedHistory: boolean;
@@ -32,6 +49,7 @@ interface PersonalData {
 export default class ChatBot extends SapphireClient {
 	pl: TauPrologInstance;
 	session: Session;
+	personalData: PersonalData;
 
 	// Snowflake is a string, but it's a string of numbers.
 	// ChatBot#sessions is a map of user IDs to their personal data.
@@ -46,35 +64,45 @@ export default class ChatBot extends SapphireClient {
 	}
 
 	async start() {}
+
+	getSymptoms(query: string) {
+		this.session.consult("../../knowledgeBase.pro");
+		this.session.query(query);
+		console.log(this.session.answers);
+	}
+
+	setPersonalData(userInput: PersonalData) {
+		this.personalData = userInput;
+
+		// Get age group
+		if (this.personalData.age < 6) this.personalData.ageGroup = "infant";
+		else if (this.personalData.age < 5 && this.personalData.age < 60) this.personalData.ageGroup = "normal";
+		else this.personalData.ageGroup = "old";
+
+		// Get BMI
+		this.personalData.bmi = Math.ceil(this.personalData.weight / (this.personalData.height / 100) ** 2);
+
+		// Check if person has high blood pressure
+		this.personalData.highBloodPressure = this.personalData.systolicBloodPressure > 140 || this.personalData.diastolicBloodPressure > 90;
+
+		// Check if person has fever
+		this.personalData.hasFever = this.personalData.bodyTemperature > 38;
+	}
+
+	getPersonalData() {
+		return this.personalData;
+	}
 }
 
 declare module "@sapphire/framework" {
 	interface SapphireClient {
 		pl: TauPrologInstance;
+		session: Session;
+		personalData: PersonalData;
 		directory: Map<Snowflake, PersonalData>;
+
+		getSymptoms(query: string): void;
+		setPersonalData(userInput: PersonalData): void;
+		getPersonalData(): PersonalData;
 	}
-}
-
-function diagnosis(session: Session) {
-	session.consult("../../knowledgeBase.pro"),
-		{
-			success: () => {
-				session.query("member(X, [a, b, c])."),
-					{
-						success: () => {},
-						fail: () => {},
-						error: (err: unknown) => {
-							if (err instanceof Error) {
-								console.log(`[ERROR in query - ChatBot.ts] ${err.message}]`);
-							}
-						},
-					};
-			},
-
-			error: (err: unknown) => {
-				if (err instanceof Error) {
-					console.log(`[ERROR in consult - ChatBot.ts] ${err.message}]`);
-				}
-			},
-		};
 }
